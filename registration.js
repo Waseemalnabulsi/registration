cdb5b0ea9e8c012b9dc501619fffe2a11b865a55
waseem <wasemon2002@gmail.com>
2025-09-15 18:15:54 +0300

    added Html,Css,Js

---
+++ registration.js
@@ -0,0 +1,12 @@
+function funsave(){
+        document.getElementById("back").style.backgroundColor="gray";
+        document.getElementById("changecolor").style.color="white";
+}
+function funcemail(){
+    alert("Email must be like:  username@gmail.com")
+}
+
+function funcolor()
+{
+    document.getElementById("color1").style.backgroundColor="yellow";
+}
